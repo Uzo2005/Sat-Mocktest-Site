3827bb32bb6265600da5e19e1f32909d1593d5f6
Uzo2005 <97878336+Uzo2005@users.noreply.github.com>
2022-04-27 23:55:39 +0100

    Delete SAT MOCK TEST SITE/instruction pages directory

--- SAT MOCK TEST SITE/instruction pages/after reading/resources/script.js
+++
@@ -1,8 +0,0 @@
-//BUTTON ALERTS
-
-function confirmAction() {
-    var popup = confirm(" Are Sure You Want To Start The Timer?");
-    if (popup == false){
-      event.preventDefault();
-    }
-  }
