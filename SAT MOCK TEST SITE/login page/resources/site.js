8a7bbaa8b0ebe75fba3b89dfa8ff65cb4e4962e9
Uzo2005 <97878336+Uzo2005@users.noreply.github.com>
2022-04-27 23:56:04 +0100

    Delete SAT MOCK TEST SITE/login page directory

--- SAT MOCK TEST SITE/login page/resources/site.js
+++
@@ -1,7 +0,0 @@
-/*GETTING ALL REQUIRED BUTTONS
-const start_btn = document.querySelector(".start_btn button");
-const info_box = document.querySelector(".info_box");
-const Exit = info_box.querySelector(".buttons .Exit");
-const Start= info_box.querySelector(".buttons .Start");
-
-//IF START QUIZ BUTTON IS CLICKED*/
