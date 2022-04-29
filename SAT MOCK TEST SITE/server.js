9c5068a7731253c6fdda417f315f6c05ec3d7889
Uzo2005 <97878336+Uzo2005@users.noreply.github.com>
2022-04-29 05:24:59 +0100

    Add files via upload

--- SAT MOCK TEST SITE/server.js
+++ SAT MOCK TEST SITE/server.js
@@ -9,6 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/login page', 'login.html'))
 });
 
-const PORT  = process.env.PORT || 2022;
+// const PORT  = process.env.PORT || 2022;
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
